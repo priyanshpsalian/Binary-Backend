d5af07c3822ffde390dd5e4585660e03af9a53a7
Priyansh Salian <79307288+priyanshpsalian@users.noreply.github.com>
2022-09-19 23:10:30 +0530

    shifted to atlas from mongo db

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017/ecommerce", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // useCreateIndex: true,
-  })
+  // .connect("mongodb://localhost:27017/ecommerce", {
+  .connect(
+    "mongodb+srv://priyanshpsalian:username1234@cluster0.0ylymgc.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    }
+  )
   .then(() => {
     console.log("connection to db is successfull");
   })

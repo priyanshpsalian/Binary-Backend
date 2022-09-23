94cb163c8268b221f9b25eaa7925141e259551be
Raghav Rathi <ytwithrvr@gmail.com>
2022-09-23 19:18:50 +0530

    form group changes

--- src/routes/Community/community_data.js
+++ src/routes/Community/community_data.js
@@ -10,94 +10,105 @@ const register_user = require("../../models/register_user");
 router.use(cors());
 const res = require("express/lib/response");
 router.get("/search/:location/:tags/:", async (req, res) => {
-  try {
-    let result = await search_group.find({
-      $or: [
-        { name: { $regex: req.params.key } },
-        { company: { $regex: req.params.key } },
-        { category: { $regex: req.params.key } },
-      ],
-    });
-    res.send(result);
-    // console.log(`${email} and ${password} and ${useremail._id}`);
-  } catch (erroe) {
-    res.status(400).send("invalid Email");
-  }
+	try {
+		let result = await search_group.find({
+			$or: [
+				{ name: { $regex: req.params.key } },
+				{ company: { $regex: req.params.key } },
+				{ category: { $regex: req.params.key } },
+			],
+		});
+		res.send(result);
+		// console.log(`${email} and ${password} and ${useremail._id}`);
+	} catch (erroe) {
+		res.status(400).send("invalid Email");
+	}
 });
 router.get("/community/:name", async (req, res) => {
-  let id = await community_data.findOne({ name: req.params.name });
-  if (id) {
-    res.send(id);
-  } else {
-    res.send("No records found");
-  }
+	let id = await community_data.findOne({ name: req.params.name });
+	if (id) {
+		res.send(id);
+	} else {
+		res.send("No records found");
+	}
 });
 router.post("/CommunityRegister", async (req, res) => {
-  try {
-    const registerUser = new community_data({
-      // user_name: req.body.user_name,
-      name: req.body.organizationname,
-      organiser: req.body.organiser,
-      description: req.body.description,
-      tags: req.body.tags,
-      date: req.body.datetime,
-      // phone: req.body.phone,
-      location: req.body.location,
-      city: req.body.city,
-      state: req.body.state,
-      country: req.body.country,
-      img: req.body.image,
-      // img: req.body.img,
-      // likes: req.body.likes,
+	console.log("request");
+	try {
+		// 		organisationname: "",
+		// organiser: "",
+		// description: "",
+		// tags: "",
+		// image: "",
+		// datetime: "",
+		// location: "",
+		// city: "",
+		// state: "",
+		// country: "",
+		console.log(req.body);
+		const registerUser = new community_data({
+			// user_name: req.body.user_name,
+			name: req.body.organisationname,
+			organiser: req.body.organiser,
+			description: req.body.description,
+			tags: req.body.tags,
+			date: req.body.datetime,
+			// phone: req.body.phone,
+			location: req.body.location,
+			city: req.body.city,
+			state: req.body.state,
+			country: req.body.country,
+			img: req.body.image,
+			// img: req.body.img,
+			// likes: req.body.likes,
 
-      // password: req.body.password,
-      // confirmpassword: req.body.reEnterPassword,
-    });
-    
+			// password: req.body.password,
+			// confirmpassword: req.body.reEnterPassword,
+		});
 
-    // const token = await registerUser.generateAuthToken();
-    // res.cookie("jwt", token, {
-    //   expires: new Date(Date.now() + 50000),
-    //   httpOnly: true,
-    // });
-    let registered = await registerUser.save();
-    // registered.toObject();
-    // delete registered.password;
-    // delete registered.confirmpassword;
-    await register_user.findOneAndUpdate(
-      {
-        firstname: `${req.body.user_name}`,
-      },
-      {
-        $addToSet: {
-          community: req.body.name,
-        },
-      }
-    );
-
-    res.status(201).send(registerUser);
-  } catch (e) {
-    res.send("error");
-  }
+		// const token = await registerUser.generateAuthToken();
+		// res.cookie("jwt", token, {
+		//   expires: new Date(Date.now() + 50000),
+		//   httpOnly: true,
+		// });
+		let registered = await registerUser.save();
+		// registered.toObject();
+		// delete registered.password;
+		// delete registered.confirmpassword;
+		// await register_user.findOneAndUpdate(
+		// 	{
+		// 		firstname: `${req.body.user_name}`,
+		// 	},
+		// 	{
+		// 		$addToSet: {
+		// 			community: req.body.name,
+		// 		},
+		// 	}
+		// );
+		console.log("bef res");
+		res.status(201).send(registerUser);
+		console.log("after resp");
+	} catch (e) {
+		res.send("error");
+	}
 });
 router.post("/CommunityChat/:name", async (req, res) => {
-  try {
-    
-    await community_data.findOneAndUpdate(
-      {
-        firstname: `${req.params.name}`,
-      },
-      {
-        $addToSet: {
-          chat: req.body,
-        },
-      }
-    );
+	try {
+		await community_data.findOneAndUpdate(
+			{
+				firstname: `${req.params.name}`,
+			},
+			{
+				$addToSet: {
+					chat: req.body,
+				},
+			}
+		);
 
-    res.status(201).send(registerUser);
-  } catch (e) {
-    res.send("error");
-  }
+		res.status(201).send(registerUser);
+	} catch (e) {
+		res.send("error");
+	}
 });
 
 module.exports = router;
